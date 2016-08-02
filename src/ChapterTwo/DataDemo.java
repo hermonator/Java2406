9016d457c64b0779b3a1c0a622caa893cb8fa773
Jesse Hermon <jesse.hermon@my.jcu.edu.au>
2016-08-02 21:18:29 +1000

    Chapter 2 DataDemo finished

---
+++ src/ChapterTwo/DataDemo.java
@@ -0,0 +1,15 @@
+package ChapterTwo;
+
+/**
+ * Created by Jesse Hermon on 2/08/2016.
+ */
+public class DataDemo {
+    public static void main(String[] args) {
+        int aWholeNumber = 315;
+        final int STATES_IN_US = 50;
+        System.out.print("The number is ");
+        System.out.println(aWholeNumber);
+        System.out.print("The number if states is ");
+        System.out.println(STATES_IN_US);
+    }
+}
