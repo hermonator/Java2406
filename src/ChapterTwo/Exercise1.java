f5c8b14294fd882d9055be5c9115d3c518ebec1b
Jesse Hermon <jesse.hermon@my.jcu.edu.au>
2016-08-03 09:16:17 +1000

    Chapter Two Exercise1 complete

--- src/ChapterTwo/Exercise1.java
+++ src/ChapterTwo/Exercise1.java
@@ -8,19 +8,33 @@ public class Exercise1 {
         // a. 4 + 6 * 2 = 16
         int a = 4 + 6 * 2;
         System.out.println(a);
-        // b. 10 / 5 + 8
+        // b. 10 / 5 + 8 = 10
         int b = 10 / 5 + 8;
         System.out.println(b);
-        // c. 12 / 4 + 16 / 2
+        // c. 12 / 4 + 16 / 2 = 11
         int c = 12 / 4 + 16 / 2;
         System.out.println(c);
-        // d. 17 / 2
-        // e. 22 / 5
-        // f. 39 / 10
-        // g. 19 % (2 + 3)
-        // h. 3 + 4 * 20 / 3
-        // i. 36 % (6 + 2)
-        // j. 8 % 2 * 0
+        // d. 17 / 2 = 8
+        int d = 17 / 2;
+        System.out.println(d);
+        // e. 22 / 5 = 4
+        int e = 22 /5;
+        System.out.println(e);
+        // f. 39 / 10 = 3
+        int f = 39 / 10;
+        System.out.println(f);
+        // g. 19 % (2 + 3) = 4
+        int g = 19 % (2 + 3);
+        System.out.println(g);
+        // h. 3 + 4 * 20 / 3 = 29
+        int h = 3 + 4 * 20 / 3;
+        System.out.println(h);
+        // i. 36 % (6 + 2) = 4
+        int i = 36 % (6 + 2);
+        System.out.println(i);
+        // j. 8 % 2 * 0 = 0
+        int j = 8 % 2 * 0;
+        System.out.println(j);
 
     }
 
---
+++ src/ChapterTwo/Exercise2.java
@@ -0,0 +1,7 @@
+package ChapterTwo;
+
+/**
+ * Created by Jesse Hermon on 3/08/2016.
+ */
+public class Exercise2 {
+}
