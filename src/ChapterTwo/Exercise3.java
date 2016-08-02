122bbe770bf00103ab1efc1644157bf04951e79d
Jesse Hermon <jesse.hermon@my.jcu.edu.au>
2016-08-03 09:50:20 +1000

    Chapter Two Exercise3 complete

---
+++ src/ChapterTwo/Exercise3.java
@@ -0,0 +1,21 @@
+package ChapterTwo;
+
+/**
+ * Created by Jesse Hermon on 3/08/2016.
+ */
+public class Exercise3 {
+    public static void main(String[] args) {
+        /*
+        a. int, 2
+        b. char, 'A'
+        c. long, 7900000000L
+        d. int, 230000000
+        e. byte, 47
+        f. short, 234
+        g. short, 150
+        h. short, 2003
+        i. byte, 8
+        j. int, 3200000
+         */
+    }
+}
