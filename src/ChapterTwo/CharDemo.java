c5963d7e9fb4fd9abc767054d1067c8a29106bd3
Jesse Hermon <jesse.hermon@my.jcu.edu.au>
2016-08-03 05:47:03 +1000

    Chapter Two CharDemo Complete

---
+++ src/ChapterTwo/CharDemo.java
@@ -0,0 +1,12 @@
+package ChapterTwo;
+
+/**
+ * Created by Jesse Hermon on 3/08/2016.
+ */
+public class CharDemo {
+    public static void main(String[] args) {
+        char initial = 'A';
+        System.out.println(initial);
+        System.out.println("\t\"abc\\def\bghi\n\njkl");
+    }
+}
