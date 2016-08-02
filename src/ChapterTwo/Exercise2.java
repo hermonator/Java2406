438b5baa0d3ab72c5c5fade2860a7d7e4640d837
Jesse Hermon <jesse.hermon@my.jcu.edu.au>
2016-08-03 09:44:58 +1000

    Chapter Two Exercise2 complete

--- src/ChapterTwo/Exercise2.java
+++ src/ChapterTwo/Exercise2.java
@@ -4,5 +4,19 @@ package ChapterTwo;
  * Created by Jesse Hermon on 3/08/2016.
  */
 public class Exercise2 {
+    public static void main(String[] args) {
+        /*
+        a. True
+        b. True
+        c. False
+        d. True
+        e. False
+        f. False
+        g. False
+        h. True
+        i. True
+        j. True
+         */
+    }
 
 }
