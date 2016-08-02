3b20d23f4c068bb4db6800108d1822a15fc3a1bb
Jesse Hermon <jesse.hermon@my.jcu.edu.au>
2016-08-02 21:30:22 +1000

    Chapter 2 IntegerDemo currently being worked on

---
+++ src/ChapterTwo/IntegerDemo.java
@@ -0,0 +1,18 @@
+package ChapterTwo;
+
+/**
+ * Created by Jesse Hermon on 2/08/2016.
+ */
+public class IntegerDemo {
+    public static void main(String[] args) {
+        int anInt = 1234;
+        byte aByte = 1234;
+        short aShort = 1234;
+        long aLong = 1234;
+
+        System.out.println("The int is " + anInt);
+        System.out.println("The byte is " + aByte);
+        System.out.println("The short is " + aShort);
+        System.out.println("The long is " +aLong);
+    }
+}
