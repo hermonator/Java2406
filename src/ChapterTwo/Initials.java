7a1e11e0ea1f4bab48487be50be0fb3077774417
Jesse Hermon <jesse.hermon@my.jcu.edu.au>
2016-08-03 10:07:32 +1000

    Chapter Two Initials complete

---
+++ src/ChapterTwo/Initials.java
@@ -0,0 +1,13 @@
+package ChapterTwo;
+
+/**
+ * Created by Jesse Hermon on 3/08/2016.
+ */
+public class Initials {
+    public static void main(String[] args) {
+        char firstInitial = 'J';
+        char middleInitial = 'P';
+        char lastInitial = 'H';
+        System.out.println(firstInitial + "." + middleInitial + "." +lastInitial + ".");
+    }
+}
