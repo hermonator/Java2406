9e19584a517bcdc28475ed33e13bd609c1a7ba2b
Jesse Hermon <jesse.hermon@my.jcu.edu.au>
2016-08-05 08:07:50 +1000

    Chapter Three TestInfo complete

---
+++ src/ChapterThree/TestInfo.java
@@ -0,0 +1,11 @@
+package ChapterThree;
+
+/**
+ * Created by Jesse Hermon on 5/08/2016.
+ */
+public class TestInfo {
+    public static void main(String[] args) {
+        System.out.println("Calling method from another class:");
+        ParadiseInfo.displayInfo();
+    }
+}
