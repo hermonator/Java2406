5b900d112d39b4856cee69a9ccdc06d6d34cc110
Jesse Hermon <jesse.hermon@my.jcu.edu.au>
2016-08-05 20:40:06 +1000

    Chapter three exercise2 completed

--- src/ChapterThree/Exercise2.java
+++ src/ChapterThree/Exercise2.java
@@ -4,4 +4,33 @@ package ChapterThree;
  * Created by Jesse Hermon on 5/08/2016.
  */
 public class Exercise2 {
+    /*
+    a
+    b
+    c
+    f
+    h
+    j
+
+     */
+    public static void main(String[] args) {
+
+
+        int age = 34;
+        int weight = 180;
+        double height = 5.9;
+
+        calculate(age,weight);
+        calculate(age, height);
+        calculate(weight, height);
+        calculate(weight, weight);
+
+
+
+    }
+
+    public static void calculate(int age, double size)
+    {
+        System.out.println("whoa");
+    }
 }
