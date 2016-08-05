43415d17c30041dc7594a71ee99c35e55be59f50
Jesse Hermon <jesse.hermon@my.jcu.edu.au>
2016-08-05 20:29:40 +1000

    Chapter three review questions completed

---
+++ src/ChapterThree/Exercise1.java
@@ -0,0 +1,7 @@
+package ChapterThree;
+
+/**
+ * Created by Jesse Hermon on 5/08/2016.
+ */
+public class Exercise1 {
+}
---
+++ src/ChapterThree/ReviewQuestions.java
@@ -0,0 +1,29 @@
+package ChapterThree;
+
+/**
+ * Created by Jesse Hermon on 5/08/2016.
+ */
+public class ReviewQuestions {
+    /*  1. b
+        2. d
+        3. c
+        4. b
+        5. d
+        6. d
+        7. b
+        8. c
+        9. a
+        10. b
+        11. b
+        12. a
+        13. b
+        14. a
+        15. c
+        16. a
+        17. c
+        18. a
+        19. d
+        20. a.
+
+     */
+}
