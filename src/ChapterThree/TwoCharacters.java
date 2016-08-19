cdb2f91982f35ceb00d157d337c377c0d082d5b1
Jesse Hermon <jesse.hermon@my.jcu.edu.au>
2016-08-19 18:35:53 +1000

    Chapter Three, MyCharacter completed

---
+++ src/ChapterThree/TwoCharacters.java
@@ -0,0 +1,26 @@
+package ChapterThree;
+
+/**
+ * Created by Jesse Hermon on 19/08/2016.
+ */
+public class TwoCharacters {
+    public static void main(String[] args) {
+        // Creating the characters
+        // Displaying the characters
+        MyCharacter firstChar = new MyCharacter();
+        firstChar.setSpecialty("Ranger");
+        firstChar.setAttack(54.9);
+        firstChar.setDefense(12);
+        attributes(firstChar);
+
+        MyCharacter secondChar = new MyCharacter();
+        secondChar.setAttack(34);
+        secondChar.setDefense(37);
+        secondChar.setSpecialty("Warrior");
+        public static void attributes(MyCharacter a){
+
+
+        }
+
+    }
+}
