d0c6d0d4d5eb35133793291f354eb3df0402c21e
Jesse <jesse.hermon@my.jcu.edu.au>
2016-08-22 17:27:59 +1000

    Chapter Three, CarlysEventPriceWithMethods completed

---
+++ src/ChapterThree/CarlysEventPriceWithMethods.java
@@ -0,0 +1,46 @@
+package ChapterThree;
+import java.util.Scanner;
+/**
+ * Created by jc259420 on 4/08/2016.
+ */
+public class CarlysEventPriceWithMethods {
+    public static void main(String[] args) {
+
+        int guestNumber = numberOfPeople();
+
+        motto();
+
+        pricing(guestNumber);
+
+
+
+    }
+    public static int numberOfPeople(){
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter the number of guests >> ");
+        int guestNumber;
+        guestNumber = input.nextInt();
+        return (guestNumber);
+    }
+    public static void motto(){
+        System.out.println("");
+        System.out.println("*************************************************");
+        System.out.println("* Carly's makes the food that makes it a party. *");
+        System.out.println("*************************************************");
+
+    }
+    public static void pricing(int guestNumber){
+        int PRICE_PER_PERSON = 35;
+        int totalPrice = guestNumber * PRICE_PER_PERSON;
+        System.out.println("Number of guests: " + guestNumber);
+        System.out.println("Price per guest: $" + PRICE_PER_PERSON);
+        System.out.println("Total pricing: $" + totalPrice);
+
+
+        if (guestNumber >= 50){
+            System.out.println("Large party status: True");
+        } else{
+            System.out.println("Large party status: False");
+        }
+    }
+}
---
+++ src/ChapterThree/Event.java
@@ -0,0 +1,7 @@
+package ChapterThree;
+
+/**
+ * Created by jc259420 on 22/08/2016.
+ */
+public class Event {
+}
