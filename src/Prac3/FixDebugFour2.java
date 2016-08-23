54ecdb69a7f7d6d9d278563915ced5e600b1062c
Jesse <jesse.hermon@my.jcu.edu.au>
2016-08-23 14:29:14 +1000

    Little changes

---
+++ src/Prac3/FixDebugFour2.java
@@ -0,0 +1,14 @@
+// Some circle statistics
+package Prac3;
+public class FixDebugFour2
+{
+   public static void main(String args[])
+   {
+      double radius = 12.6;
+      System.out.println("Circle statistics");
+      double area = java.lang.Math.PI * radius * radius;
+      System.out.println("area is " + area);
+      double diameter = 2 * radius;
+      System.out.println("diameter is " + diameter);
+   }
+}
