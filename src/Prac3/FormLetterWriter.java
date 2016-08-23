b62a1588ebaff6c35c251cb6ccae8cd310bf20a9
Jesse <jesse.hermon@my.jcu.edu.au>
2016-08-23 14:55:48 +1000

    Practical 3 finished

---
+++ src/Prac3/FormLetterWriter.java
@@ -0,0 +1,24 @@
+package Prac3;
+
+/**
+ * Created by jc259420 on 23/08/2016.
+ */
+public class FormLetterWriter {
+    public static void main(String[] args) {
+        String lastName = "Catter";
+        displaySalutation(lastName);
+        String firstName = "Bob";
+        displaySalutation(firstName,lastName);
+
+    }
+    public static void displaySalutation(String lastName){
+        String surname = lastName;
+        System.out.println("Dear Mr. or Ms. " + surname);
+    }
+    public static void displaySalutation(String firstName,String lastName){
+        String surname = lastName;
+        String name = firstName;
+
+        System.out.println("Dear " + name + " " + surname);
+    }
+}
