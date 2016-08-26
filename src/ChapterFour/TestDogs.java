6ee7d864629cc53c9e17c42230437ca39b41855d
Jesse <jesse.hermon@my.jcu.edu.au>
2016-08-26 12:25:21 +1000

    Chapter Four, TestDogs complete.

---
+++ src/ChapterFour/TestDogs.java
@@ -0,0 +1,15 @@
+package ChapterFour;
+
+/**
+ * Created by jc259420 on 26/08/2016.
+ */
+public class TestDogs {
+    public static void main(String[] args) {
+        DogTriathlonParticipant dog1 = new DogTriathlonParticipant("Bowser",2,85,89,0);
+        dog1.display();
+        DogTriathlonParticipant dog2 = new DogTriathlonParticipant("Rush",3,78,72,80);
+        dog2.display();
+        DogTriathlonParticipant dog3 = new DogTriathlonParticipant("Ginger",3,90,86,72);
+        dog3.display();
+    }
+}
