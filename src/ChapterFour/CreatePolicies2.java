0a9e2a7163d5689b995ff84865824e8253e76e35
Jesse <jesse.hermon@my.jcu.edu.au>
2016-08-26 11:13:18 +1000

    Chapter Four, CreatePolicies2 completed.

--- src/ChapterFour/CreatePolicies2.java
+++ src/ChapterFour/CreatePolicies2.java
@@ -3,13 +3,13 @@ package ChapterFour;
 /**
  * Created by jc259420 on 26/08/2016.
  */
-public class CreatePolicies {
+public class CreatePolicies2 {
     public static void main(String[] args) {
 
 
-        CarInsurancePolicy first = new CarInsurancePolicy(123);
-        CarInsurancePolicy second = new CarInsurancePolicy(456, 4);
-        CarInsurancePolicy third = new CarInsurancePolicy(789,12, "Newcastle");
+        CarInsurancePolicy2 first = new CarInsurancePolicy2(123);
+        CarInsurancePolicy2 second = new CarInsurancePolicy2(456, 4);
+        CarInsurancePolicy2 third = new CarInsurancePolicy2(789,12, "Newcastle");
 
         // This does not work due to the overriden contructors not including a no input variation.
         // CarInsurancePolicy fourth = new CarInsurancePolicy();
