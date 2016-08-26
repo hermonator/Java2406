3be36ca5dbf9d6b601acbdb3c7c7b772d64932ab
Jesse <jesse.hermon@my.jcu.edu.au>
2016-08-26 12:20:31 +1000

    Chapter Four, DogTriathlonParticipant complete.

--- src/ChapterFour/DogTriathlonParticipant.java
+++ src/ChapterFour/DogTriathlonParticipant.java
@@ -13,5 +13,23 @@ public class DogTriathlonParticipant {
     private int agilityScore;
     private int total;
     private double avg;
-    
+
+    public DogTriathlonParticipant(String name, int numEvents, int score1, int score2, int score3){
+        this.name = name;
+        NUM_EVENTS = numEvents;
+        obedienceScore = score1;
+        conformationScore = score2;
+        agilityScore = score3;
+
+        total = obedienceScore + conformationScore + agilityScore;
+        avg = (double) total / NUM_EVENTS;
+        totalCumulativeScore = totalCumulativeScore + total;
+    }
+
+    public void display(){
+        System.out.println(name + " participated in " + NUM_EVENTS + " events and has an average score of " + avg);
+        System.out.println(" " + name + " has a total score of " + total + " bringing the total cumulative score to " +
+        totalCumulativeScore);
+    }
+
 }
