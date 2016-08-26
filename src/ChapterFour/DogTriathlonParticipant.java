472bbb9f247eefb0e4a117f20a2d4abc6a7d7336
Jesse <jesse.hermon@my.jcu.edu.au>
2016-08-26 11:52:24 +1000

    Chapter Four, DogTriathlonParticipant underway

---
+++ src/ChapterFour/DogTriathlonParticipant.java
@@ -0,0 +1,17 @@
+package ChapterFour;
+
+/**
+ * Created by jc259420 on 26/08/2016.
+ */
+public class DogTriathlonParticipant {
+    private final int NUM_EVENTS;
+    private static int totalCumulativeScore = 0;
+
+    private String name;
+    private int obedienceScore;
+    private int conformationScore;
+    private int agilityScore;
+    private int total;
+    private double avg;
+    
+}
